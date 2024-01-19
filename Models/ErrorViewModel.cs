c4e847d1c847b288474843a3ea8ee201744f597a
Per Simen Reed <persr18@uia.no>
2024-01-19 13:28:07 +0100

    Added new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ikt206g24v_01.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
